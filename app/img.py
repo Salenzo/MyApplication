dd0ed6e7adc433921c7b0d95bf959e12d1efd494
AKHIA <50533365+akhialomgir@users.noreply.github.com>
2021-09-03 10:07:48 +0800

    Update img.py

--- app/img.py
+++ app/img.py
@@ -1,40 +1,46 @@
 import cv2
-from config import cfg
+from app.config import cfg
 
 PATH = './config.ini'
 
+
 class image(object):
-    def __init__(self, origin_path, save_path, threshold) -> None:
+    def __init__(self, origin_path, threshold) -> None:
         super().__init__()
         CFG = cfg(PATH)
         h, w = CFG.read('ui', 'window_size').split('x', 1)
 
-        img = cv2.imread(origin_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, self.img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-        self.save_path = save_path
-        self.height = h
-        self.width = w
+        self.origin_path = origin_path
+        self.threshold = threshold
+        self.height = int(h)
+        self.width = int(w)
         self.point1 = (0, 0)
         self.point2 = (0, 0)
-        self.cut_img = None
 
-    def get_image_roi(self):
+        self.refresh()
+
+    def refresh(self) -> None:
+        img = cv2.imread(self.origin_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, self.img = cv2.threshold(
+            img, self.threshold, 255, cv2.THRESH_BINARY)
+
+    def crop(self, save_path) -> None:
         cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('image', self.width, self.height)
+        cv2.resizeWindow('image', self.height, self.width)
         while True:
             cv2.setMouseCallback('image', self.__on_mouse)
             cv2.imshow('image', self.img)
             key = cv2.waitKey(0)
-            if key != None:
+            if key is not None:
                 break
         cv2.destroyAllWindows()
         try:
-            cv2.imwrite(self.save_path, self.cut_img)
+            cv2.imwrite(save_path, self.cut_img)
         except Exception as err:
             print(err)
 
-    def __on_mouse(self, event, x, y, flags, param):
+    def __on_mouse(self, event, x, y, flags, param) -> None:
         img2 = self.img.copy()
         if event == cv2.EVENT_LBUTTONDOWN:
             self.point1 = (x, y)
@@ -58,3 +64,26 @@ class image(object):
                 cut_img = self.img[min_y:min_y + height, min_x:min_x + width]
                 self.cut_img = cut_img
                 cv2.imshow('ROI', cut_img)
+
+    def sift(self, template_path):
+        sift = cv2.xfeatures2d.SIFT_create()
+        template = cv2.imread(template_path)
+        img = self.img
+        kp1, des1 = sift.detectAndCompute(template, None)
+        kp2, des2 = sift.detectAndCompute(img, None)
+
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1, des2, k=2)
+
+        good = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append([m])
+        img5 = cv2.drawMatchesKnn(template, kp1, img, kp2, good, None, flags=2)
+        # TODO get the roi region from the matches and return
+        # show the roi region's similarity with the template
+        cv2.namedWindow('BFmatch', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('BFmatch', self.height, self.width)
+        cv2.imshow('BFmatch', img5)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()

dbe1f7ad546ec7813844a35007f098f1c98f4095
AKHIA <50533365+akhialomgir@users.noreply.github.com>
2021-09-06 06:53:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from app.adb import ADBClient
 from app.img import image
 
 IMG = image('./app/img/a_3.jpg', 140)
-IMG.crop('./app/template/b.png')
-IMG.sift('./app/template/b.png')
+# IMG.crop('./app/template/a.png')
+IMG.sift('./app/template/a.png')

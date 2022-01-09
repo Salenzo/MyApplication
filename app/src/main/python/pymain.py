9711e32eb3c1145901fabc8a90b21a63d1a6a084
Frog Chen <satgo1546@yeah.net>
2022-01-09 15:32:32 +0800

    尝试增加输入输出重定向但是失败了
    大量多线程问题

--- app/src/main/python/pymain.py
+++ app/src/main/python/pymain.py
@@ -3,6 +3,9 @@ import sys
 import math
 import ctypes
 import traceback
+from queue import Queue
+from io import TextIOBase
+from java.android.stream import BytesOutputWrapper
 
 # 保留测试用
 def aaa(a):
@@ -16,6 +19,41 @@ def kill_thread(tid):
     print("kill_thread = " + str(ret))
     return ret
 
+real_stdout = sys.stdout
+real_stderr = sys.stderr
+
+class ConsoleOutputStream(TextIOBase):
+    def __init__(self, service, stream, style):
+        self.stream = stream
+        self.service = service
+        self.style = style
+        self.buffer = BytesOutputWrapper(self)
+
+    def __repr__(self):
+        return f"<ConsoleOutputStream {self.stream}>"
+
+    @property
+    def encoding(self):
+        return self.stream.encoding
+
+    @property
+    def errors(self):
+        return self.stream.errors
+
+    def writable(self):
+        return self.stream.writable()
+
+    def write(self, s):
+        # Pass the write to the underlying stream first, so that if it throws an exception,
+        # the app crashes in the same way whether it's using ConsoleOutputStream or not (chaquo/chaquopy #5712).
+        result = self.stream.write(s)
+        if len(s) > 0:
+            self.service.log(s, self.style)
+        return result
+
+    def flush(self):
+        self.stream.flush()
+
 def main(service):
     try:
         #import time
@@ -23,9 +61,14 @@ def main(service):
         #    adb.swipe(400,1000,400,400,0.05)
         #    adb.screenshot() # 可能返回None，如果不是空那应该是个数组吧，我也不知道具体是什么
         #    time.sleep(0.5)
+        sys.stdout = ConsoleOutputStream(service, real_stdout, 1)
+        sys.stderr = ConsoleOutputStream(service, real_stderr, 2)
         exec(open(os.path.join(os.environ["HOME"], "more.py")).read(), {"adb": service})
     except SystemExit:
         pass
     except Exception as e:
         with open(os.path.join(os.environ["HOME"], "more.py"), "w") as f:
-            f.write("\"\"\"" + traceback.format_exc() + "\"\"\"")
+            f.write("r\"\"\"" + traceback.format_exc().replace("\"\"\"", "\\\"\"\"") + "\n\"\"\"")
+    sys.stdout = real_stdout
+    sys.stderr = real_stderr
+    service.log("Python止步于此。", 16)

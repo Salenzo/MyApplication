e39f69d84300942955f6db70eae92334941d45bb
Frog Chen <satgo1546@yeah.net>
2022-01-16 19:41:05 +0800

    感觉两线交点不太行
    这是因为浮点误差吗？不像。更可能是拟合直线的不精确吧。

--- py/prts.py
+++ py/prts.py
@@ -1,6 +1,7 @@
-from urllib.request import HTTPPasswordMgrWithPriorAuth
+from itertools import combinations
 import cv2
 import numpy as np
+import math
 
 # 根据目标高度，锁定原图比例缩放。
 def resize(img, height, **kwargs):
@@ -15,6 +16,20 @@ def video_between_two(video_filename, img0, img1):
     for x in range(0, width, width // 500):
         video.write(np.hstack((img0[:, :x], img1[:, x:])))
     video.release()
+
+# 求两直线a0 -- b0和a1 -- b1的交点，各点以元组给出，共计八个数值。
+def line_line_intersection(a0, b0, a1, b1):
+    A0 = b0[1] - a0[1]
+    B0 = a0[0] - b0[0]
+    C0 = a0[0] * A0 + a0[1] * B0
+    A1 = b1[1] - a1[1]
+    B1 = a1[0] - b1[0]
+    C1 = a1[0] * A1 + a1[1] * B1
+    det = A0 * B1 - A1 * B0
+    if abs(det) > 1e-8:
+        return (C0 * B1 - C1 * B0) / det, (C1 * A0 - C0 * A1) / det
+    else:
+        return None
 
 # 从通常截图与选中干员的子弹时间中的截图推断选中干员带来的视角变化。
 def bullet_time_transform(img0, img1):
@@ -38,17 +53,25 @@ def bullet_time_transform(img0, img1):
     #video_between_two("114.mp4", cv2.warpPerspective(img0, homography, (width, height)), img1)
     return homography
 
-def eee(img0, img1):
+def eee(img0, img1, img2, img3):
     homography = bullet_time_transform(img0, img1)
     height, width = img0.shape[:2]
     x0, y0 = height * 2 // 3, height // 12
     x1, y1 = width - height // 3, height * 4 // 5
-    img1 = cv2.warpPerspective(img1, homography, (width, height), flags=cv2.WARP_INVERSE_MAP)
-    img1 = cv2.divide(img1.astype(np.uint16) * 256, img0, dtype=cv2.CV_8U)
-    img1 = cv2.GaussianBlur(img1, (0, 0), 2)
-    img1 = cv2.inRange(img1, np.array([128, 255, 128], dtype=np.uint8), np.array([240, 255, 240], dtype=np.uint8))
-    cv2.imwrite("out11.png", img1[y0:y1, x0:x1])
+    img2, img3 = [cv2.warpPerspective(img, homography, (width, height), flags=cv2.WARP_INVERSE_MAP) for img in [img2, img3]]
+    img1 = cv2.inRange(cv2.cvtColor(cv2.absdiff(img2, img3), cv2.COLOR_BGR2GRAY), 1, 255)
+    img1 = cv2.Canny(img1, 50, 200, None, 3)
+    lines = cv2.HoughLinesP(img1, 1, math.pi / 180, 50, None, height // 24, height // 12)
+    if lines is None:
+        lines = []
+    img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+    print(lines)
+    for [l] in lines:
+        cv2.line(img1, (l[0], l[1]), (l[2], l[3]), (224, 175, 102), 3, cv2.LINE_AA)
+    points = [p for p in [line_line_intersection((l0[0], l0[1]), (l0[2], l0[3]), (l1[0], l1[1]), (l1[2], l1[3])) for l0, l1 in combinations([line for [line] in lines if abs(line[1] - line[3]) > 2], 2)] if p is not None and abs(p[0] - width // 2) < height // 240]
+    print(list(points))
+    cv2.imwrite("out11.png", img1)
     #cv2.imshow("", img2)
     #cv2.waitKey()
 
-print(eee(cv2.imread("b1.jpg"), cv2.imread("b2.jpg")))
+print(eee(cv2.imread("b1.png"), cv2.imread("b2.png"), cv2.imread("b3.png"), cv2.imread("b4.png")))

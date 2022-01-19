4abf5f73db38b25ae02a537ea67129cb6940ee19
Frog Chen <satgo1546@yeah.net>
2022-01-19 15:31:47 +0800

    bottom.py：识别中间一丢丢阴阳

--- py/bottom.py
+++ py/bottom.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('b1.png')
 h, w = img.shape[:2]
@@ -8,6 +9,7 @@ h, w = gray.shape[:2]
 tagArea = gray[int(0.025*h):int(0.20*h),] 
 ret,thresh = cv2.threshold(tagArea,127,255,cv2.THRESH_BINARY)
 
+print(np.where(np.diff(np.sum(thresh // 200, axis=0, dtype=np.int32)) == -thresh.shape[0])[0] + 1)
 #cv2.imwrite('b.png',gray)
 
 #cv2.imshow('', thresh)

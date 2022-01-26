ef15591283ff7b44c54adbcd2705b0c3a07355c2
Frog Chen <satgo1546@yeah.net>
2022-01-26 13:30:30 +0800

    迫真60FPS

--- py/server.py
+++ py/server.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 app.jinja_env.auto_reload = True
 
-socketio = SocketIO(app)
+io = SocketIO(app)
 
 
 @app.route('/')
@@ -13,30 +12,25 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/send')
-def send_message():
-    sid = request.args.get('sid')
-    message = request.args.get('message')
-    socketio.send(message, to=sid)
-    return {'status': 'ok'}
-
-
-@socketio.on('connect')
+@io.on('connect')
 def connect():
     print('connect')
-    socketio.send({'sid': request.sid})
 
 
-@socketio.on('disconnect')
+@io.on('disconnect')
 def disconnect():
     print('disconnect')
 
 
-@socketio.on('message')
-def handle_message(data):
-    print(data)
-    socketio.send({'data': data})
+t = 1
+
+
+@io.on('update')
+def update():
+    global t
+    t += 1
+    io.emit('update', t)
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    io.run(app, debug=True)
